29f2ae89dcb764279e446ad0caf50814589a8faf
omrilotan <omri@hamadgera.com>
2021-11-02 16:23:52 +0000

    Add support to lists in data structure

--- tests/test_resolve.py
+++ tests/test_resolve.py
@@ -2,6 +2,8 @@ from pyresolve import resolve
 
 
 my_dictionary = {"outer": {"middle": {"inner": "Balue"}}}
+dictionary_with_list = {"list": ["one", "two", list("abcdefghijklmnop")]}
+users_list = {"users": [{"name":"Joe"}, {"name":"Jane"}]}
 
 
 def test_happy_flow():
@@ -18,3 +20,23 @@ def test_fallback_value_false():
 
 def test_fallback_value_list():
     assert resolve(my_dictionary, "outer.missing.something", []) == []
+
+
+def test_number_cell():
+    assert resolve(dictionary_with_list, "outer.1") is None
+
+
+def test_dictionary_with_list_number():
+    assert resolve(dictionary_with_list, "list.1") is "two"
+
+
+def test_dictionary_with_list_nested():
+    assert resolve(dictionary_with_list, "list.2.012") is "m"
+
+
+def test_dictionary_with_list_missing():
+    assert resolve(dictionary_with_list, "list.3") is None
+
+
+def test_dictionary_list_dictionary():
+    assert resolve(users_list, "users.1.name") is "Jane"
